7be6e75d9bc130a639f317f729d92554472b68e0
mancinilucas <mancini.lucasg@gmail.com>
2023-01-15 11:28:14 -0300

    recuperar dados do BD

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,34 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        //Conecta ao BD
+        Connection conn = null;
+        //Realiza a consulta ao BD
+        Statement st = null;
+        //Armazena o resultado em uma var
+        ResultSet rs = null;
+
+        try{
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
